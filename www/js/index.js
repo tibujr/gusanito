269d553a907bf400834d09c163cb0f58858f33f6
tibujr <maycol_to@hotmail.com>
2014-03-20 18:25:22 -0500

    cambiando de index

--- www/js/index.js
+++ www/js/index.js
@@ -33,17 +33,24 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+        navigator.geolocation.getCurrentPosition(app.onSuccess, app.onError);
+    }
+
+    onSuccess: function(position){
+        var longitude = position.coords.longitude;
+        var latitude = position.coords.latitude;
+        var latLong = new google.maps.LatLng(latitude, longitude);
+
+        var mapOptions = {
+            center: latLong,
+            zoom: 16,
+            mapTypeId: google.maps.mapTypeId.ROADMAP  //MapTypeId.ROADMAP
+        };
+
+        var map=new google.maps.Map(document.getElementById("geolocation"), mapOptions);
     },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+    onError: function(error){
+        alert('code: '      + error.code    + '\n' + 'message: ' + error.message + '\n');
     }
 };
